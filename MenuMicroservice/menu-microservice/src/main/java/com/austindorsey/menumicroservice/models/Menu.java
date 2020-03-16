8dceb492f4b9875819e4a2c80e293f3f88801c20
Austin Dorsey <austin_dorsey@hotmail.com>
2020-03-16 16:54:59 -0500

    Auto generating getters,setters, constructors, ect for menu and menuItem

--- MenuMicroservice/menu-microservice/src/main/java/com/austindorsey/menumicroservice/models/Menu.java
+++ MenuMicroservice/menu-microservice/src/main/java/com/austindorsey/menumicroservice/models/Menu.java
@@ -7,4 +7,80 @@ public class Menu {
     String name;
     String items;
     Date revisionDate;
+
+    public Menu(int id, String name, String items, Date revisionDate) {
+        this.id = id;
+        this.name = name;
+        this.items = items;
+        this.revisionDate = revisionDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getItems() {
+        return items;
+    }
+
+    public void setItems(String items) {
+        this.items = items;
+    }
+
+    public Date getRevisionDate() {
+        return revisionDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu [id=" + id + ", items=" + items + ", name=" + name + ", revisionDate=" + revisionDate + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((items == null) ? 0 : items.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((revisionDate == null) ? 0 : revisionDate.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Menu other = (Menu) obj;
+        if (id != other.id)
+            return false;
+        if (items == null) {
+            if (other.items != null)
+                return false;
+        } else if (!items.equals(other.items))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (revisionDate == null) {
+            if (other.revisionDate != null)
+                return false;
+        } else if (!revisionDate.equals(other.revisionDate))
+            return false;
+        return true;
+    }
 }
--- MenuMicroservice/menu-microservice/src/main/java/com/austindorsey/menumicroservice/models/MenuItem.java
+++ MenuMicroservice/menu-microservice/src/main/java/com/austindorsey/menumicroservice/models/MenuItem.java
@@ -9,4 +9,111 @@ public class MenuItem {
     String discription;
     Number cost;
     Date revisionDate;
+
+    public MenuItem(int id, String catagory, String name, String discription, Number cost, Date revisionDate) {
+        this.id = id;
+        this.catagory = catagory;
+        this.name = name;
+        this.discription = discription;
+        this.cost = cost;
+        this.revisionDate = revisionDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getCatagory() {
+        return catagory;
+    }
+
+    public void setCatagory(String catagory) {
+        this.catagory = catagory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDiscription() {
+        return discription;
+    }
+
+    public void setDiscription(String discription) {
+        this.discription = discription;
+    }
+
+    public Number getCost() {
+        return cost;
+    }
+
+    public void setCost(Number cost) {
+        this.cost = cost;
+    }
+
+    public Date getRevisionDate() {
+        return revisionDate;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((catagory == null) ? 0 : catagory.hashCode());
+        result = prime * result + ((cost == null) ? 0 : cost.hashCode());
+        result = prime * result + ((discription == null) ? 0 : discription.hashCode());
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((revisionDate == null) ? 0 : revisionDate.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MenuItem other = (MenuItem) obj;
+        if (catagory == null) {
+            if (other.catagory != null)
+                return false;
+        } else if (!catagory.equals(other.catagory))
+            return false;
+        if (cost == null) {
+            if (other.cost != null)
+                return false;
+        } else if (!cost.equals(other.cost))
+            return false;
+        if (discription == null) {
+            if (other.discription != null)
+                return false;
+        } else if (!discription.equals(other.discription))
+            return false;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (revisionDate == null) {
+            if (other.revisionDate != null)
+                return false;
+        } else if (!revisionDate.equals(other.revisionDate))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem [catagory=" + catagory + ", cost=" + cost + ", discription=" + discription + ", id=" + id
+                + ", name=" + name + ", revisionDate=" + revisionDate + "]";
+    }
 }
