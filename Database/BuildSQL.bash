db2fac4f735d26e77364e97acdb35b6f56dff425
Austin Dorsey <austin_dorsey@hotmail.com>
2020-03-20 23:58:14 -0500

    Menu docker setup

---
+++ Database/BuildSQL.bash
@@ -0,0 +1,4 @@
+docker build -t pizza-shop-mysql ./Database
+docker run -d --name pizza-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=admin123 pizza-shop-mysql
+docker network create pizza-shop
+docker network connect pizza-shop pizza-mysql
