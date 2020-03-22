440f84b3a01f7b4bdabee70cd7954d504fc83c49
Austin Dorsey <austin_dorsey@hotmail.com>
2020-03-21 22:55:53 -0500

    Creating bash file to run all integration tests.

---
+++ IntegrationTests/RunAllIntegrationTests.bash
@@ -0,0 +1,12 @@
+cd InventoryIntegrationTests
+py InventoryAPIIntegrationTest.py
+
+cd ../AutoRestockerIntegrationTests
+py AutoRestockerIntegrationTest.py
+
+cd ../CustomerIntegrationTests
+py CustomerAPIIntegrationTest.py
+
+cd ../MenuIntegrationTests
+py MenuAPIIntegrationTest-Menu.py
+py MenuAPIIntegrationTest-MenuItem.py
