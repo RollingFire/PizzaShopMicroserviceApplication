ac76589488f632ea2bbe1248c541a07ee1abc7a0
Austin Dorsey <austin_dorsey@hotmail.com>
2020-04-11 12:06:08 -0500

    [Integration] Adding echos to make logs more readable when running all tests

--- IntegrationTests/RunAllIntegrationTests.bash
+++ IntegrationTests/RunAllIntegrationTests.bash
@@ -1,12 +1,16 @@
+echo "--------------------Inventory--------------------"
 cd InventoryIntegrationTests
 py InventoryAPIIntegrationTest.py
 
+echo "--------------------AutoRestocker--------------------"
 cd ../AutoRestockerIntegrationTests
 py AutoRestockerIntegrationTest.py
 
+echo "--------------------Customer--------------------"
 cd ../CustomerIntegrationTests
 py CustomerAPIIntegrationTest.py
 
+echo "--------------------Menu--------------------"
 cd ../MenuIntegrationTests
 py MenuAPIIntegrationTest-Menu.py
 py MenuAPIIntegrationTest-MenuItem.py
