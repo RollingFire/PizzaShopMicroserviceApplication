d38c28c37245a107b4a9a2208151dac486ddeb34
Austin Dorsey <austin_dorsey@hotmail.com>
2020-04-11 11:19:55 -0500

    Changing restocker container name

--- PizzaShopBuildRun.bash
+++ PizzaShopBuildRun.bash
@@ -14,7 +14,7 @@ docker build -t pizza-shop-order-api ./OrderMicroservice/order-microservice
 
 docker run -d --name pizza-mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=admin123 pizza-shop-mysql
 docker run -d --name pizza-inventory-api -p 9093:9093 pizza-shop-inventory-api
-winpty docker run -d -t --name pizza-shop-auto-restocker pizza-shop-auto-restocker
+winpty docker run -d -t --name pizza-auto-restocker pizza-shop-auto-restocker
 docker run -d --name pizza-customer-api -p 9092:9092 pizza-shop-customer-api
 docker run -d --name pizza-menu-api -p 9095:9095 pizza-shop-menu-api
 docker run -d --name pizza-recipe-api -p 9096:9096 pizza-shop-recipe-api
@@ -23,7 +23,7 @@ docker run -d --name pizza-order-api -p 9094:9094 pizza-shop-order-api
 docker network create pizza-shop
 docker network connect pizza-shop pizza-mysql
 docker network connect pizza-shop pizza-inventory-api
-docker network connect pizza-shop pizza-shop-auto-restocker
+docker network connect pizza-shop pizza-auto-restocker
 docker network connect pizza-shop pizza-customer-api
 docker network connect pizza-shop pizza-menu-api
 docker network connect pizza-shop pizza-recipe-api
